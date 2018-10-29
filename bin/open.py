c3430b370fa44082f9c9d482e72b78f47139680a
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-10-30 01:06:58 +0800

    added main feature for population phase, expolore TODAG. Still need to figure out how to properly print cards though

--- bin/open.py
+++ bin/open.py
@@ -33,13 +33,54 @@ def write_cards(cards):
         pickle.dump(cards, data)
 
 
+def find_rewards(cards):
+    """
+    traverse the DAG and find all nodes with a reward
+
+    return: list[uuid] with reward cards
+    """
+    return [card_id for card_id, _card in cards.items() if _card.is_reward]
+
+
+def get_parents(cards, todo):
+    """
+    given a todo find all the parents of the todo and return them
+    i.e. backtrack on the DAG
+
+    Args:
+        cards: global dict of card instances
+        todo: (uuid) of the todo
+
+    Returns:
+        parents: list[int, list[int, list[int]], ..]
+            the format for the return value is unusual, for
+            visualization reason every time the DAG branches during the
+            backtrack, one whole branch (list[int]) while be a single
+            element of the list, instead of appending all the elements
+            in the branch to the parents list
+    """
+    res = []
+    curr = todo
+    while CARDS.get(curr) and CARDS[curr].parents:
+        if len(CARDS[curr].parents) == 1:
+            curr = CARDS[curr].parents[0]
+            res.append(curr)
+        else:
+            last = []
+            for parent in CARDS[curr].parents:
+                last.append([parent] + get_parents(cards, parent))
+            return res + [last]
+    return res
+
+
 if __name__ == "__main__":
     sys.path.append('../TODAG')
     from card import card
     import uuid
     CARDS = load_cards()
     print_cards()
-    print "\n\n[A] new card\n[B] add parent\n[C] delete card\n"
+    print "\n\n[A] new card\n[B] add parent\n[C] delete card\n\
+    [D] explore decision tree\n"
     got = raw_input()
     if got == 'A':
         new_card = card()
@@ -65,6 +106,24 @@ if __name__ == "__main__":
             exit("error: CARD NOT EXISTING")
         del(CARDS[uuid.UUID(read_id)])
         print "Card deleted succesfully"
+    elif got == 'D':
+        print "Rewards on the DAG:\n"
+        rewards = find_rewards(CARDS)
+        for index, reward in enumerate(rewards):
+            print "[{}]".format(index)
+            CARDS[reward].detail()
+        print "\n"
+        print "Choose reward card you want backtrack from"
+        got_index = int(raw_input())
+        if got_index in range(len(rewards)):
+            reward_id = rewards[got_index]
+            print "\n[REWARD]"
+            CARDS[reward_id].detail()
+            parents_id = get_parents(CARDS, reward_id)
+            print parents_id  # TODO: figure out how to print this stuff
+        else:
+            exit("error: reward index out of range")
+
     else:
         exit("error: NOT IMPLEMENTED")
     write_cards(CARDS)
